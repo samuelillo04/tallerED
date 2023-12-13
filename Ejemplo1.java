b3bb2b13782ca9ddceb61bcdd88f2290a74ca13f
samuelillo04 <samiinfate04@gmail.com>
2023-12-13 12:55:14 +0100

    primer commit

---
+++ Ejemplo1.java
@@ -0,0 +1,16 @@
+package es.studium.ejemplo1;
+
+public class Ejemplo1  {  
+
+
+
+	public static void main(String[] args)
+	{
+	
+			System.out.println("Hola mundo");
+			System.out.println("Buenas");
+
+
+	}
+	
+}
---
+++ HolaMundo.java
@@ -0,0 +1,19 @@
+package es.studium.ejemplo1;
+
+public class HolaMundo  {  
+
+
+
+	public static void main(String[] args)
+	{
+	
+		System.out.println("Hola mundo");
+		System.out.println("Buenas");
+		System.out.print("buenos dias");
+		System.out.println();
+		
+		
+
+	}
+	
+}
